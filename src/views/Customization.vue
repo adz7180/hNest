16bd37a0afbf05dad4b011c723ca8345fe942f41
adz7180 <adamhaymour@gmail.com>
2025-06-19 11:18:16 -0600

    Add files via upload

---
+++ src/views/Customization.vue
@@ -0,0 +1,26 @@
+import CustomizationPanel from '@/components/CustomizationPanel.vue';
+<!-- src/views/Customization.vue -->
+<template>
+  <div class="customization">
+    <h2>Customize Your Home</h2>
+    <!-- Customization Panel Placeholder -->
+    <CustomizationPanel />
+  </div>
+</template>
+
+<script>
+import CustomizationPanel from "@/components/CustomizationPanel.vue";
+
+export default {
+  name: "CustomizationView",
+  components: {
+    CustomizationPanel
+  }
+};
+</script>
+
+<style scoped>
+.customization {
+  padding: 20px;
+}
+</style>
